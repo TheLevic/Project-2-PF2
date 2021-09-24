d4d7916df57be80d94f217fef80fc6951110c8c0
TheLevic <levicrider08@gmail.com>
2021-09-24 16:42:25 -0500

    First commit

---
+++ mystrtok.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char *find_first_in_the_string_that_is_not_in_the_set(char *str, const char *set);
+char *find_first_in_the_string_that_is_in_the_set(char *str, const char *set);
+char *mystrtok(char *str, const char *delim);
