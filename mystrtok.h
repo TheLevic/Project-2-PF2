0ad6477ac4e915c31a61bf46d86c6bbf6c61b306
TheLevic <levicrider08@gmail.com>
2021-09-25 19:41:37 -0500

    Getting everything ready, about to start implementing strtok

--- mystrtok.h
+++ mystrtok.h
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include "helper.h"
 using namespace std;
 
-char *find_first_in_the_string_that_is_not_in_the_set(char *str, const char *set);
-char *find_first_in_the_string_that_is_in_the_set(char *str, const char *set);
 char *mystrtok(char *str, const char *delim);
