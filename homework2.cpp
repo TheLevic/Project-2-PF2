51a9872d85f8a152c6b4b02027b5e29fb86c62c6
TheLevic <levicrider08@gmail.com>
2021-09-27 19:05:44 -0500

    Semi-Working

--- homework2.cpp
+++ homework2.cpp
@@ -1,5 +1,8 @@
 #include "mystrtok.h"
+    
 
 int main(){
-    
+    char str[] = "Hello Friend";
+    char delim[] = " ";
+    cout << mystrtok(str,delim);
 }
--- mystrtok.cpp
+++ mystrtok.cpp
@@ -3,19 +3,24 @@
 #include "mystrtok.h"
 using namespace std;
 
+/*
+*Levi Crider
+*9/27/2021
+*/
 
-char *mystrtok(char *str, const char *delim){
+
+char *mystrtok(char *str, const char* delim){
     static char* p; //To keep our spot
     char* curr_str; //The current string we are working with
     char* start; //Starting Position
     char* end; //End position
-    if (*str != NULL){
-        curr_str = str; //Setting the current string pointer = to the actual string since the first variable is != to null
+    if (*str != '\0'){
+        curr_str = str;//Setting the current string pointer = to the actual string since the first variable is != to null
     }
     else{
-        curr_str = p; //If the first char is = to null than we willse the curr_str = to the tracker.
+        curr_str = p;//If the first char is = to null than we willse the curr_str = to the tracker.
     }
-    if (curr_str != NULL){
+    if (*curr_str != '\0'){
         start = find_first_in_the_string_that_is_not_in_the_set(curr_str,delim); //Getting start location
         if (start == NULL){
             p = NULL;
@@ -23,9 +28,9 @@ char *mystrtok(char *str, const char *delim){
 
         } 
         end = find_first_in_the_string_that_is_in_the_set(start,delim); //Getting the end location
-        if (end != NULL){
+        if (*end != '\0'){
                 *end = '\0'; //Setting the location of the delimiter = to NULL
-                p = end+1; //Setting the tracker = to one past the new NULL character
+                p = end++; //Setting the tracker = to one past the new NULL character
         }
         else{ //When end does = NULL
                 *p = '\0';
